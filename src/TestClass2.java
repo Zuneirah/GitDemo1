c67dae8220ae420fbeea7b3cdc5648a9b052505c
Abdul <uha24278@gmail.com>
2024-06-21 20:48:39 +0530

    firstcommit

---
+++ src/TestClass2.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class TestClass2 {
+	public static void main(String[] args) throws IOException {
+		System.out.println("New Test Class File 2 Added");
+	}
+
+
+}
